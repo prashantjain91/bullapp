272d6be94465948e6c2b5e1ae5216befcdcffdb6
unknown <Admin@EMLP-MUM0016.aurum.com>
2022-06-20 17:55:23 +0530

    New App Created

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Home = () => (
 <div>
-    <h2>This is the home section</h2>  
+    <h2>This is the home section</h2>
+    <p>There were issues with our github repository so we created new</p>
 </div>
 )
 
